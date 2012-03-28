4a48c13e00556d60af1e09e907dbe6ccd9c4a0b8
Johannes Brodwall <johannes@brodwall.com>
2012-03-28 12:44:42 +0200

    Tried to make regular expressions koans easier to understand. Now depends on knowledge of methods, though

--- src/test/scala/org/scalakoans/AboutRegularExpressions.scala
+++ src/test/scala/org/scalakoans/AboutRegularExpressions.scala
@@ -1,11 +1,12 @@
 package org.scalakoans
 
 import support.KoanSuite
+import scala.util.matching._
 
 class AboutRegularExpressions extends KoanSuite {
 
   koan ("regular expressions match substrings") {
-    val expression = """wor""".r
+    val expression:Regex = """wor""".r
     expression.findFirstIn("hello world").orNull should be (__)
   }
 
@@ -15,23 +16,19 @@ class AboutRegularExpressions extends KoanSuite {
   }
 
   koan ("regular expressions can be used in matchers") {
-    val expression1 = "hello there".r
-    val expression2 = "hello world".r
-    var result:String = null
-    
-    val theString = "hello world"
-    
-    theString match {
-      case expression1() => result = "there"
-      case expression2() => result = "world"
-      case _ =>             result = "no match"
+    def tryToMatch(str:String) = {
+      val expression1 = "hello there".r
+      val expression2 = "hello world".r
+      
+      str match {
+        case expression1() => "there"
+        case expression2() => "world"
+        case _ =>             "no match"
+      }
     }
-
-    result should be(__)
-    meditate
-    // change theString to make each of the following pass
-    // result should be("there")
-    // result should be("no match")
+    tryToMatch("hello world") should be(__)
+    tryToMatch("hello " + __) should be("there")
+    tryToMatch("hello " + __) should be("no match")
   }
 
   koan ("expressions can extract values") {
@@ -52,7 +49,7 @@ class AboutRegularExpressions extends KoanSuite {
   koan ("match zero characters") {
     val expression = """h(a*)llo""".r
     "hllo" match {
-      case expression(s) => s should be(__)
+      case expression(s) => s should be("")
       case _ => fail()
     }
     meditate
@@ -78,6 +75,17 @@ class AboutRegularExpressions extends KoanSuite {
     val numericMatch = """the answer is (\d+)""".r
     "the answer is 123" match {
        case numericMatch(number) => number should be(__)
+       case _ => fail("should match")
+    }
+  }
+  
+  koan ("partial binding") {
+    val numericMatch = """the (.*) is (\d+)""".r
+    "the answer is 123" match {
+       case numericMatch("answer", number) =>
+          number should be(__)
+       case numericMatch("question", number) =>
+          fail
        case _ => fail("should match")
     }
   }
